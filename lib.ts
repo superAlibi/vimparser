6b0c2aa6753c4f09e10b94a600d47034b21f41a1
xujiaming <alibi_jia@foxamil.com>
2024-05-22 22:12:17 +0800

    提交到jsr

--- lib.ts
+++ lib.ts
@@ -1,28 +1,57 @@
 export class RangeInfo {
-  constructor(public startOffset: bigint, public endOffset: bigint) { }
+  constructor(
+    public startOffset: bigint,
+    public endOffset: bigint,
+    public offsetStart: number | bigint = 0,
+  ) {}
 
   get length() {
-    return this.endOffset - this.startOffset
+    return this.endOffset - this.startOffset;
   }
-
 }
-
-
+async function reqRange(
+  source: string,
+  startByte: number | bigint,
+  endByte: number | bigint,
+  startOffset: number | bigint = 0n,
+) {
+  if (endByte < startByte) return;
+  const start = Number(startByte),
+    end = Number(endByte),
+    offset = Number(startOffset);
+  const resp = await globalThis.fetch(source, {
+    headers: {
+      range: [start + offset, end + offset].join("-"),
+    },
+  });
+  if (!resp.ok || !resp.body) {
+    return;
+  }
+  /**
+   * todo
+   */
+  const result = new Uint8Array(end - start);
+  let count = 0;
+  for await (const u8arr of resp.body) {
+    for (const iterator of u8arr) {
+      result[count++] = iterator;
+    }
+  }
+  return result;
+}
 export const enum BEFASTMetaStage {
-  INIT = 'init',
-  HEADERREQING = 'headerReqing',
-  HEADERFEILD = 'headerFeild',
-  HEADERREQED = 'headerReqed',
-  RANGEREQING = 'rangeReqing',
-  RANGEREQFEILD = 'rangeFeild',
-  RANGEREQED = 'rangereqed',
+  INIT = "init",
+  HEADERREQING = "headerReqing",
+  HEADERFEILD = "headerFeild",
+  HEADERREQED = "headerReqed",
+  RANGEREQING = "rangeReqing",
+  RANGEREQFEILD = "rangeFeild",
+  RANGEREQED = "rangereqed",
   NAMEDATABLOCKREQING = "namedatablockreqing",
   NAMEDATABLOCKREQFEILD = "namedatablockreqFeild",
   NAMEDATABLOCKREQED = "namedatablockreqed",
 }
-/**
- * 
- */
+/** */
 export abstract class BFASTMeta extends EventTarget {
   /**
    * 当前阶段
@@ -35,38 +64,38 @@ export abstract class BFASTMeta extends EventTarget {
    * rangeNameReq:请求dataBlockName中
    * rangeNameFeild:请求dataBlockName中
    */
-  public stage: string = BEFASTMetaStage.INIT
+  public stage: string = BEFASTMetaStage.INIT;
   /**
    * vim头信息
    */
-  private fileHeader?: BigInt64Array
+  private fileHeader?: BigInt64Array;
   /**
    * 数据区块开始位置(bit)
    */
   get dataBlockStartOffset() {
-    return ((this.fileHeader?.at(1)) ?? 0n)
+    return ((this.fileHeader?.at(1)) ?? 0n);
   }
   get globalDataBlockStartOffset() {
-    return this.dataBlockStartOffset + BigInt(this.offset)
+    return this.dataBlockStartOffset + BigInt(this.offset);
   }
   /**
    * 数据区块结束位置(bit)
    */
   get dataBlockEndOffset() {
-    return (this.fileHeader?.at(2) ?? 0n)
+    return (this.fileHeader?.at(2) ?? 0n);
   }
   get globalDataBlockEndOffset() {
-    return this.dataBlockEndOffset + BigInt(this.offset)
+    return this.dataBlockEndOffset + BigInt(this.offset);
   }
   /**
    * 数据区块总数量
    */
   get dataBlockCount() {
-    return (this.fileHeader?.at(3)) ?? 0n
+    return (this.fileHeader?.at(3)) ?? 0n;
   }
   /**
    * 数据区块索引范围
-   * @description 
+   * @description
    * [
    * bigint,bigint, // first blockData start offset, first blockData end offset
    * bigint,bigint,  // second blockData start offset, second blockData end offset
@@ -79,212 +108,371 @@ export abstract class BFASTMeta extends EventTarget {
    * bigint,bigint, // n th blockData start offset, n th  blockData end offset
    * ]
    */
-  public rangeData?: BigInt64Array
+  public rangeData?: BigInt64Array;
   /**
-  * bfast 数据块名称位置索引
-  */
+   * bfast 数据块名称位置索引
+   */
   get dataBlocksNameOffset() {
-    return this.rangeData?.slice(0, 2)
+    return this.rangeData?.slice(0, 2);
   }
-
 
   /**
    * 数据区块名称和其位置影射
    */
-  public dataBlockNameOffsetMap: Map<string, RangeInfo> = new Map()
+  public dataBlockNameOffsetMap: Map<string, RangeInfo> = new Map();
   /**
-   * 
    * @param source vim请求路径
    * @param offset 子区块的偏移量,
    * 因为bfast格式的文件子区块有可能还是bfast格式,所以,当取用子区块时中的bfast区块时,需要加上整个区块相对于整个文件的偏移量
    */
   constructor(public source: string, public offset: number = 0) {
-    super()
+    super();
   }
   /**
    * 请求文件的指定区间
-   * @param startByte 
-   * @param endByte 
-   * @returns 
+   * @param startByte
+   * @param endByte
+   * @returns
    */
-  protected async reqRange(startByte: number | bigint, endByte: number | bigint) {
-    if (endByte < startByte) { return }
-    const start = Number(startByte),
-      end = Number(endByte)
-    const resp = await globalThis.fetch(this.source, {
-      headers: {
-        range: [start + this.offset, end + this.offset].join('-')
-      }
-    });
-    if (!resp.ok || !resp.body) {
-      return
-    }
-    /**
-     * todo
-     */
-    const result = new Uint8Array(end - start)
-    let count = 0
-    for await (const u8arr of resp.body) {
-
-      for (const iterator of u8arr) {
-        result[count++] = iterator
-      }
-
-    }
-    return result
+  protected reqRange(start: number | bigint, end: number | bigint) {
+    return reqRange(this.source, start, end, this.offset);
   }
   async initMeta() {
     if (!this.fileHeader) {
-
-
-      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERREQING))
-      const result = await this.reqRange(0, 32).catch(e => {
+      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERREQING));
+      const result = await this.reqRange(0, 32).catch((e) => {
         console.error(e);
-        this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERFEILD))
-        return void 0
-      })
-      if (!result) { return }
-      this.fileHeader = new BigInt64Array(result!.buffer, 0, 4)
-      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERREQED))
+        this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERFEILD));
+        return void 0;
+      });
+      if (!result) return;
+      this.fileHeader = new BigInt64Array(result!.buffer, 0, 4);
+      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.HEADERREQED));
     }
 
     if (!this.rangeData) {
-
-      const end = Number(this.dataBlockStartOffset)
-      const start = end - Number(this.dataBlockCount) * 16
-      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.RANGEREQING))
-      const rangeResult = await this.reqRange(start, end).then((r) => { return r }).catch(e => {
+      const end = Number(this.dataBlockStartOffset);
+      const start = end - Number(this.dataBlockCount) * 16;
+      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.RANGEREQING));
+      const rangeResult = await this.reqRange(start, end).then((r) => {
+        return r;
+      }).catch((e) => {
         console.error(e);
-        this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.RANGEREQFEILD))
-        return void 0
-      })
+        this.dispatchEvent(
+          new Event(this.stage = BEFASTMetaStage.RANGEREQFEILD),
+        );
+        return void 0;
+      });
 
-      if (!rangeResult) { return }
-      this.rangeData = new BigInt64Array(rangeResult!.buffer!, 0, Number(this.dataBlockCount) * 2)
-      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.RANGEREQED))
+      if (!rangeResult) return;
+      this.rangeData = new BigInt64Array(
+        rangeResult!.buffer!,
+        0,
+        Number(this.dataBlockCount) * 2,
+      );
+      this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.RANGEREQED));
     }
-    const dataRange = this.rangeData!.slice(2)
-    const textDecoder = new TextDecoder()
-    this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQING))
+    const dataRange = this.rangeData!.slice(2);
+    const textDecoder = new TextDecoder();
+    this.dispatchEvent(
+      new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQING),
+    );
     const nameRange = await this.reqRange(
       Number(this.dataBlocksNameOffset?.at(0)),
-      Number(this.dataBlocksNameOffset?.at(1)))
-      .catch((e) => {
-        console.error(e);
-        this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQFEILD))
-        return void 0
-      })
-    this.dispatchEvent(new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQED))
+      Number(this.dataBlocksNameOffset?.at(1)),
+    ).catch((e) => {
+      console.error(e);
+      this.dispatchEvent(
+        new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQFEILD),
+      );
+      return void 0;
+    });
+    this.dispatchEvent(
+      new Event(this.stage = BEFASTMetaStage.NAMEDATABLOCKREQED),
+    );
 
+    const rangeName = textDecoder.decode(nameRange).split("\0").filter(Boolean);
 
-    const rangeName = textDecoder.decode(nameRange).split('\0').filter(Boolean)
-
-
-    for (let index = 0, length = dataRange.length / 2; index < length; index++) {
-
+    for (
+      let index = 0, length = dataRange.length / 2;
+      index < length;
+      index++
+    ) {
       const start = dataRange.at(index * 2 + 0);
       const end = dataRange.at(index * 2 + 1);
-      this.dataBlockNameOffsetMap.set(rangeName.at(index)!, new RangeInfo(start!, end!))
-
+      this.dataBlockNameOffsetMap.set(
+        rangeName.at(index)!,
+        new RangeInfo(start!, end!),
+      );
     }
   }
 }
-
+type VimDataBlockFormat = "u8arr" | "string";
 export class VIMFileMeta extends BFASTMeta {
-  public textDecoder = new TextDecoder()
+  public textDecoder = new TextDecoder();
 
   constructor(source: string) {
-    super(source)
+    super(source);
   }
   get nameRange() {
-    return new RangeInfo(this.dataBlockStartOffset!, this.dataBlockEndOffset!)
+    return new RangeInfo(this.dataBlockStartOffset!, this.dataBlockEndOffset!);
   }
 
   /**
    * vim文件其他数据区块范围
    */
   get dataBlockRangeOffset() {
-    return this.rangeData?.slice(2)
+    return this.rangeData?.slice(2);
   }
-  async initMeta(): Promise<void> {
-    await super.initMeta()
-
-
-
-  }
-
 
   /**
    * 给出指定命名区块数据
    * @param dataBlockName 区块名称
    * @param format 数据格式
-   * @returns 
+   * @returns
    */
-  async getDataBlock(dataBlockName: string, format: 'string' | 'u8arr' = 'u8arr') {
-    const rangeInfo = this.dataBlockNameOffsetMap.get(dataBlockName)
-    if (!rangeInfo) return Promise.resolve(void 0)
+  async getDataBlock(
+    dataBlockName: string,
+    format: VimDataBlockFormat = "u8arr",
+  ) {
+    const rangeInfo = this.dataBlockNameOffsetMap.get(dataBlockName);
+    if (!rangeInfo) return Promise.resolve(void 0);
 
-    const u8arr = await this.reqRange(Number(rangeInfo.startOffset), Number(rangeInfo.endOffset))
-    if (format === 'u8arr') return u8arr
-    if (!u8arr) return void 0
-    return this.textDecoder.decode(u8arr)
+    const u8arr = await this.reqRange(
+      Number(rangeInfo.startOffset),
+      Number(rangeInfo.endOffset),
+    );
+    if (format === "u8arr") return u8arr;
+    if (!u8arr) return void 0;
+    return this.textDecoder.decode(u8arr);
   }
   getGeometry() {
-    const rangeInfo = this.dataBlockNameOffsetMap.get('geometry')
-    if (!rangeInfo) return void 0
-    return new GeometryWithG3D(this.source, Number(rangeInfo.startOffset))
+    const rangeInfo = this.dataBlockNameOffsetMap.get("geometry");
+    if (!rangeInfo) return void 0;
+    return new VIMGeometry(this.source, Number(rangeInfo.startOffset));
   }
-  getHeader(format: 'u8arr' | 'string') {
-    return this.getDataBlock('header', format)
+  /**
+   * @param format
+   * @returns
+   */
+  getHeader(format: VimDataBlockFormat) {
+    return this.getDataBlock("header", format);
   }
-  async getStrings(format: 'u8arr' | 'string') {
-    const rangeInfo = this.dataBlockNameOffsetMap.get('strings')
-    if (!rangeInfo) return void 0
-    const u8arr = await this.reqRange((rangeInfo.startOffset), rangeInfo.endOffset)
-    if (format === 'u8arr') {
-      return u8arr
-    }
-    return this.textDecoder.decode(u8arr)
+  /**
+   * @param format
+   * @returns
+   */
+  getStrings(format: VimDataBlockFormat) {
+    return this.getDataBlock("strings", format);
   }
 }
-
 
 /**
  * vim 文件中存在的子区块:geometry区块
  */
-export class GeometryWithG3D extends BFASTMeta {
+class VIMGeometryMeta extends BFASTMeta {
   constructor(souce: string, offset: number) {
-    super(souce, offset)
+    super(souce, offset);
   }
-  #meta?: Uint8Array
+  #meta?: Uint8Array;
+
   async getMetaInfo() {
-    if (this.#meta) return this.#meta
-    const metaRange = this.dataBlockNameOffsetMap.get('meta')
-    
-    const metau8arr = this.#meta = await this.reqRange(metaRange?.startOffset ?? 0n, metaRange?.endOffset ?? 0n)
-      .catch(e => {
+    if (this.#meta) return this.#meta;
+    const metaRange = this.dataBlockNameOffsetMap.get("meta");
+
+    const metau8arr = this.#meta = await this.reqRange(
+      metaRange?.startOffset ?? 0n,
+      metaRange?.endOffset ?? 0n,
+    )
+      .catch((e) => {
         console.error(e);
-        return void 0
-      })
+        return void 0;
+      });
     if (!metau8arr) {
-
-      return
+      return;
     }
-    return this.#meta = metau8arr
+    return this.#meta = metau8arr;
   }
-  async initMeta(): Promise<void> {
-    await super.initMeta()
+}
+
+type AssociationType =
+  | "vertex"
+  | "corner"
+  // | "face"
+  // | "edge"
+  | "mesh"
+  | "submesh"
+  // | "ffer"
+  | "instance"
+  | "all"
+  | "shapevertex"
+  | "shape"
+  | "material";
+type SemanticType =
+  | "unknown"
+  | "position"
+  | "index"
+  | "indexoffset"
+  | "vertexoffset"
+  | "fset"
+  | "normal"
+  | "binormal"
+  | "tangent"
+  | "materialid"
+  | "visibility"
+  | "size"
+  | "uv"
+  | "color"
+  | "smoothing"
+  | "weight"
+  | "mapchannel"
+  | "ces"
+  | "id"
+  | "joint"
+  | "boxes"
+  | "spheres"
+  | "user";
+type TargetArrayType =
+  | "int8"
+  | "int16"
+  | "int32"
+  | "int64"
+  | "uint8"
+  | "uint16"
+  | "uint32"
+  | "uint64"
+  | "float32"
+  | "float64";
+
+interface GeometryAttrbuteMetaOption {
+  rangeInfo: RangeInfo;
+  transframTarget: TargetArrayType;
+  itemSize?: number;
+  index?: number;
+  source: string;
+}
+class GeometryAttributeMeta {
+  public rangeInfo: RangeInfo;
+  public transframTarget: TargetArrayType;
+  public itemSize: number;
+  public source: string;
+  index: number = 0;
+  #u8arr?: Uint8Array;
+  constructor(options: GeometryAttrbuteMetaOption) {
+    const { rangeInfo, transframTarget, itemSize, index, source } = options;
+    this.rangeInfo = rangeInfo;
+    this.transframTarget = transframTarget;
+    this.itemSize = itemSize || 1;
+    this.index = index || 0;
+    this.source = source;
   }
 
+  async getTypedArray() {
+    if (!this.#u8arr) {
+      this.#u8arr = await reqRange(
+        this.source,
+        this.rangeInfo.startOffset,
+        this.rangeInfo.endOffset,
+        this.rangeInfo.startOffset,
+      );
+    }
+    const u8arr = this.#u8arr;
+    if (!u8arr) {
+      console.error('异常数据：在元信息中存在数据区间，但实际请求却无法拿到数据');
+      return;
+    }
+
+    switch (this.transframTarget) {
+      case "int8":
+        return new Int8Array(u8arr?.buffer, 0, u8arr.length);
+      case "int16":
+        return new Int16Array(u8arr?.buffer, 0, u8arr.length / 2);
+      case "int32":
+        return new Int32Array(u8arr?.buffer, 0, u8arr.length / 4);
+      case "int64":
+        return new BigInt64Array(u8arr?.buffer, 0, u8arr.length / 8);
+      case "uint8":
+        return new Uint8Array(u8arr?.buffer, 0, u8arr.length);
+      case "uint16":
+        return new Uint16Array(u8arr?.buffer, 0, u8arr.length / 2);
+      case "uint32":
+        return new Uint32Array(u8arr?.buffer, 0, u8arr.length / 4);
+      case "uint64":
+        return new BigUint64Array(u8arr?.buffer, 0, u8arr.length / 8);
+      case "float32":
+        return new Float32Array(u8arr?.buffer, 0, u8arr.length / 4);
+      case "float64":
+        return new Float32Array(u8arr?.buffer, 0, u8arr.length / 8);
+    }
+  }
+}
+
+export class VIMGeometry extends VIMGeometryMeta {
+  get geoMetaEntries() {
+    return Array.from(this.dataBlockNameOffsetMap.entries()).filter(([k]) =>
+      k !== "meta"
+    ).map(
+      ([keys, rangeinfo]) => {
+        const [_, association, semantic, index, dataType, dataArity] = keys
+          .split(":") as [
+            "g3d",
+            AssociationType,
+            SemanticType,
+            string,
+            TargetArrayType,
+            string,
+          ];
+        return {
+          association,
+          semantic,
+          index,
+          dataType,
+          dataArity,
+          rangeinfo,
+        };
+      },
+    );
+  }
+  /**
+   * 给出某元属性的子信息映射，逻辑上来讲，信息映射的值应该只有一个元素
+   * @param associationType
+   * @returns
+   */
+  getAssociation(associationType: AssociationType) {
+    const result = this.geoMetaEntries.filter((i) =>
+      i.association === associationType
+    );
+    const associationMetaMap: Partial<
+      Record<SemanticType, GeometryAttributeMeta>
+    > = {};
+    for (const iterator of result) {
+      const { semantic, index, dataType, dataArity, rangeinfo } = iterator;
+      if (associationMetaMap[semantic]) {
+        throw TypeError("存在重复的分类子信息");
+      }
+      const attr = new GeometryAttributeMeta({
+        rangeInfo: new RangeInfo(
+          rangeinfo.startOffset,
+          rangeinfo.endOffset,
+          this.offset,
+        ),
+        transframTarget: dataType,
+        itemSize: Number(dataArity),
+        index: Number(index),
+        source: this.source,
+      });
+      associationMetaMap[semantic] = attr;
+    }
+
+    return associationMetaMap;
+  }
 }
 
 export class AssetWithBFAST extends BFASTMeta {
   constructor(souce: string, offset: number) {
-    super(souce, offset)
+    super(souce, offset);
   }
   async initMeta(): Promise<void> {
-    await super.initMeta()
-
+    await super.initMeta();
   }
-
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,41 @@
-import { assertEquals } from "jsr:@std/assert";
-import { add } from "./main.ts";
+import { BEFASTMetaStage, VIMFileMeta } from "./lib.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const bfastFile = new VIMFileMeta("http://localhost:8080/assets/spanish.vim");
+bfastFile.addEventListener(BEFASTMetaStage.HEADERREQED, (_v) => {
+  console.log("vim区块范围：");
+
+  console.table({
+    数据块位置: {
+      开始位置: bfastFile.dataBlockEndOffset,
+      结束位置: bfastFile.dataBlockEndOffset,
+    },
+  });
 });
+bfastFile.addEventListener(BEFASTMetaStage.RANGEREQING, (_v) => {
+  console.log("查询数据区块");
+});
+bfastFile.addEventListener(BEFASTMetaStage.NAMEDATABLOCKREQING, (_v) => {
+  console.log("查询数据区块名称");
+});
+await bfastFile.initMeta();
+console.log("vim文件区块信息");
+console.table(
+  Object.fromEntries(Array.from(bfastFile.dataBlockNameOffsetMap.entries())),
+);
+
+const geometryBfast = bfastFile.getGeometry();
+
+await geometryBfast?.initMeta();
+console.log("g3d区块信息: ");
+
+console.table(
+  Object.fromEntries(
+    Array.from(geometryBfast?.dataBlockNameOffsetMap.entries() || []),
+  ),
+);
+console.log("meta信息\n", await geometryBfast?.getMetaInfo());
+const info = geometryBfast?.getAssociation("material");
+console.log('geometry:mesh:material元数据');
+
+console.table(info);
+// console.log(await info?.color?.getTypedArray());
